78f6436f94aa111648b706ad4d4983e22c7e45ab
Mirnes <mirnesademaj@gmail.com>
2021-08-28 00:34:20 +0200

    Config cleanup

--- components/Button/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./button.cjs.production.min.js')
-} else {
-  module.exports = require('./button.cjs.development.js')
-}
